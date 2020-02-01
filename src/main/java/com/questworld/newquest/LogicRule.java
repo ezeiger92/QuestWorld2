6951b56f72d631573be10b697f7964fdf62883af
ezeiger92 <ezeiger92@gmail.com>
2020-01-31 23:41:33 -0500

    Reimplement outline of LogicRule

--- src/main/java/com/questworld/newquest/LogicRule.java
+++ src/main/java/com/questworld/newquest/LogicRule.java
@@ -10,6 +10,20 @@ public class LogicRule {
 	private LogicRule() {	
 	}
 	
+	// Not a good instance holder
+	public static class RuleList {
+		public List<Integer> instanceIds;
+	}
+	
+	public static class RulePair {
+		public int leftId;
+		public int rightId;
+	}
+	
+	public static class RuleRef {
+		public int instanceId;
+	}
+	
 	public static class And extends Rule {
 		public And() {
 			super(MakeRuleKey("and"));
@@ -30,61 +44,124 @@ public class LogicRule {
 		}
 	}
 	
-	// Not a good instance holder
-	public static class RuleList {
-		public List<Integer> instanceIds;
-	}
-	
-	
-	
-	/*public static class Or extends Rule {
-		private final Rule[] rules;
-		
-		public Or(Rule... rules) {
+	public static class Or extends Rule {
+		public Or() {
 			super(MakeRuleKey("or"));
-			this.rules = rules;
+		}
+
+		@Override
+		public boolean test(Event someEvent, NodeConfig config, Profile profile) {
+			RuleResultEvent event = (RuleResultEvent) someEvent;
+			RuleList list = config.deserialize(RuleList.class);
+			
+			for(int i : list.instanceIds) {
+				if(i == event.getInstanceId() && event.getResult().isAllowed()) {
+					return true;
+				}
+			}
+			
+			return false;
 		}
 	}
 	
 	public static class Not extends Rule {
-		private final Rule rule;
-		
-		public Not(Rule rule) {
+		public Not() {
 			super(MakeRuleKey("not"));
-			this.rule = rule;
+		}
+
+		@Override
+		public boolean test(Event someEvent, NodeConfig config, Profile profile) {
+			RuleResultEvent event = (RuleResultEvent) someEvent;
+			RuleRef other = config.deserialize(RuleRef.class);
+			
+			if(event.getInstanceId() == other.instanceId) {
+				return !event.getResult().isAllowed();
+			}
+			
+			return false;
 		}
 	}
 	
 	public static class Xor extends Rule {
-		private final Rule rule1;
-		private final Rule rule2;
-		
-		public Xor(Rule rule1, Rule rule2) {
+		public Xor() {
 			super(MakeRuleKey("xor"));
-			this.rule1 = rule1;
-			this.rule2 = rule2;
+		}
+
+		@Override
+		public boolean test(Event someEvent, NodeConfig config, Profile profile) {
+			RuleResultEvent event = (RuleResultEvent) someEvent;
+			RulePair rules = config.deserialize(RulePair.class);
+			
+			if(event.getInstanceId() == rules.leftId) {
+				// A MESS; NOT HOW THIS WILL WORK
+				boolean other = Boolean.parseBoolean(profile.getData("state." + rules.rightId));
+				
+				return other ^ event.getResult().isAllowed();
+			}
+			else if(event.getInstanceId() == rules.rightId) {
+				boolean other = Boolean.parseBoolean(profile.getData("state." + rules.leftId));
+				
+				return other ^ event.getResult().isAllowed();
+			}
+			
+			// Not true or false: not applicable
+			return false;
 		}
 	}
 	
 	public static class Iff extends Rule {
-		private final Rule rule1;
-		private final Rule rule2;
-		
-		public Iff(Rule rule1, Rule rule2) {
+		public Iff() {
 			super(MakeRuleKey("iff"));
-			this.rule1 = rule1;
-			this.rule2 = rule2;
+		}
+
+		@Override
+		public boolean test(Event someEvent, NodeConfig config, Profile profile) {
+			RuleResultEvent event = (RuleResultEvent) someEvent;
+			RulePair rules = config.deserialize(RulePair.class);
+			
+			if(event.getInstanceId() == rules.leftId) {
+				boolean other = Boolean.parseBoolean(profile.getData("state." + rules.rightId));
+				
+				return other ^ !event.getResult().isAllowed();
+			}
+			else if(event.getInstanceId() == rules.rightId) {
+				boolean other = Boolean.parseBoolean(profile.getData("state." + rules.leftId));
+				
+				return other ^ !event.getResult().isAllowed();
+			}
+			
+			return true;
 		}
 	}
 	
 	public static class Implies extends Rule {
-		private final Rule rule1;
-		private final Rule rule2;
-		
-		public Implies(Rule rule1, Rule rule2) {
+		public Implies() {
 			super(MakeRuleKey("implies"));
-			this.rule1 = rule1;
-			this.rule2 = rule2;
 		}
-	}*/
+
+		@Override
+		public boolean test(Event someEvent, NodeConfig config, Profile profile) {
+			RuleResultEvent event = (RuleResultEvent) someEvent;
+			RulePair rules = config.deserialize(RulePair.class);
+			
+			if(event.getInstanceId() == rules.leftId) {
+				if(!event.getResult().isAllowed()) {
+					return true;
+				}
+				
+				return Boolean.parseBoolean(profile.getData("state." + rules.rightId));
+			}
+			else if(event.getInstanceId() == rules.rightId) {
+				boolean other = Boolean.parseBoolean(profile.getData("state." + rules.leftId));
+				
+				if(!other) {
+					return true;
+				}
+				
+				return event.getResult().isAllowed();
+			}
+			
+			return true;
+		}
+	}
 }
